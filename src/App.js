1b9dca2d3aaf860f3054792cbad2158da5b1902b
Charlene <charlenelcapili@gmail.com>
2023-03-08 02:49:02 -0500

    Layout updates

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
-import NoMatch from './pages/NoMatch';
+import NotFound from './pages/NotFound';
 
 import './assets/css/index.css';
 
@@ -21,7 +21,7 @@ export default function App() {
           <Route path='about' element={<About />} />
           <Route path='projects' element={<Projects />} />
           <Route path='contact' element={<Contact />} />
-          <Route path='*' element={<NoMatch />} />
+          <Route path='*' element={<NotFound />} />
         </Route>
       </Routes>
       
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,27 +1,10 @@
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
 
 export default function Layout() {
   return (
     <div>
-      {/* A 'layout route' is a good place to put markup you want to
-          share across all the pages on your site, like navigation. */}
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/about'>About</Link>
-          </li>
-          <li>
-            <Link to='/projects'>Projects</Link>
-          </li>
-          <li>
-            <Link to='/contact'>Contact</Link>
-          </li>
-        </ul>
-      </nav>
-
+      <Header />
       <Outlet />
     </div>
   );
