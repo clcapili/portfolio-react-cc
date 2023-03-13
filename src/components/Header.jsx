6789668e621b652cbc61e41ba188c30bb08d939f
Charlene <charlenelcapili@gmail.com>
2023-03-12 22:42:21 -0400

    Added NavLink component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,49 +5,54 @@ export default function Header() {
   let activeClassName = "active";
 
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive ? activeClassName : undefined
-            }
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="about"
-            className={({ isActive }) =>
-              isActive ? activeClassName : undefined
-            }
-          >
-            About
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="projects"
-            className={({ isActive }) =>
-              isActive ? activeClassName : undefined
-            }
-          >
-            Projects
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="contact"
-            className={({ isActive }) =>
-              isActive ? activeClassName : undefined
-            }
-          >
-            Contact
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <div className="container">
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? activeClassName : undefined
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="about"
+                className={({ isActive }) =>
+                  isActive ? activeClassName : undefined
+                }
+              >
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="projects"
+                className={({ isActive }) =>
+                  isActive ? activeClassName : undefined
+                }
+              >
+                Projects
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="contact"
+                className={({ isActive }) =>
+                  isActive ? activeClassName : undefined
+                }
+              >
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+    
   );
 }
