031f9cd7ce50a9c5752e001e7a4ee091a6758747
Charlene <charlenelcapili@gmail.com>
2023-03-14 16:37:41 -0400

    Added GSAP

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 
 import Layout from './components/Layout';
 
+import TransitionComponent from './components/Transition';
+
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
@@ -12,7 +14,7 @@ import NotFound from './pages/NotFound';
 import './assets/css/styles.css';
 import './assets/css/fonts.css';
 
-import { headerScript } from './../src/assets/js/scripts.js';
+import { headerScript } from './assets/js/scripts.js';
 
 export default function App() {
   // load header script
@@ -21,11 +23,46 @@ export default function App() {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path='about' element={<About />} />
-        <Route path='projects' element={<Projects />} />
-        <Route path='contact' element={<Contact />} />
-        <Route path='*' element={<NotFound />} />
+        <Route
+          index
+          element={
+            <TransitionComponent>
+              <Home />
+            </TransitionComponent>
+          }
+        />
+        <Route 
+          path='about' 
+          element={
+            <TransitionComponent>
+              <About />
+            </TransitionComponent>
+          }
+        />
+        <Route
+          path='projects' 
+          element={
+          <TransitionComponent>
+            <Projects />
+          </TransitionComponent>
+          }
+        />
+        <Route 
+          path='contact' 
+          element={
+            <TransitionComponent>
+              <Contact />
+            </TransitionComponent>
+          }
+        />
+        <Route
+          path='*' 
+          element={
+            <TransitionComponent>
+              <NotFound />
+            </TransitionComponent>
+          } 
+        />
       </Route>
     </Routes>
   );
