be5041163bfbc04114492faa3978c97e6344c1e3
Charlene <charlenelcapili@gmail.com>
2023-03-14 19:29:53 -0400

    About page markup updates and style fixes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,41 +7,54 @@ export default function About() {
         </div>
       </section>
         
-      <section className='container'>
-        <div className='row'>
-          <div className='col-md-5'>
-            <div className='skillset'>
-              <h6>Programming Languages</h6>
-              <ul>
-                <li>HTML5 & CSS3/SASS</li>
-                <li>JavaScript &#40;ES6&#41;</li>
-                <li>PHP, GraphQL</li>
-              </ul>
+      <section className='about-me'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-5'>
+              <div className='skills'>
+                <div className='skillset'>
+                  <h6>Programming Languages</h6>
+                  <ul>
+                    <li>HTML5 & CSS3/SASS</li>
+                    <li>JavaScript &#40;ES6&#41;</li>
+                    <li>PHP, GraphQL</li>
+                  </ul>
+                </div>
+
+                <div className='skillset'>
+                  <h6>Libraries & Frameworks</h6>
+                  <ul>
+                    <li>jQuery, React.js, Node.js</li>
+                    <li>Bootstrap, Foundation</li>
+                  </ul>
+                </div>
+
+                <div className='skillset'>
+                  <h6>Tools & Platforms</h6>
+                  <ul>
+                    <li>Git, Webpack</li>
+                    <li>WordPress, RestAPI</li>
+                  </ul>
+                </div>
+
+                <div className='skillset'>
+                  <h6>Design</h6>
+                  <ul>
+                    <li>Adobe XD & Figma</li>
+                    <li>Adobe Illustrator & Photoshop</li>
+                  </ul>
+                </div>
+              </div>
             </div>
 
-            <div className='skillset'>
-              <h6>Libraries & Frameworks</h6>
-              <ul>
-                <li>jQuery, React.js, Node.js</li>
-                <li>Bootstrap, Foundation</li>
-              </ul>
+            <div className='col-md-7'>
+              <p>My name is Charlene Capili, and I consider myself as someone who is creative combined with a strong and logical mind. As a result, I developed passion for interactive design and web development. I am a problem-solver, and a very organized person with high attention to detail. I enjoy designing and coding things from scratch and bringing concepts to life on the internet, as well as passion for  UI design, brand identity, and design systems, as I value intuitive designs and thoughtful interactions.</p>
+              <h1>digital tools.</h1>
             </div>
-
-            <div className='skillset'>
-              <h6>Tools & Platforms</h6>
-              <ul>
-                <li>Git, Webpack</li>
-                <li>WordPress, RestAPI</li>
-              </ul>
-            </div>
-          </div>
-
-          <div className='col-md-7'>
-            <p>My name is Charlene Capili, and I consider myself as someone who is creative combined with a strong and logical mind. As a result, I developed passion for interactive design and web development. I am a problem-solver, and a very organized person with high attention to detail. I enjoy designing and coding things from scratch and bringing concepts to life on the internet, as well as passion for  UI design, brand identity, and design systems, as I value intuitive designs and thoughtful interactions.</p>
-            <h1>digital tools.</h1>
           </div>
         </div>
       </section>
+      
     </div>
   );
 }
