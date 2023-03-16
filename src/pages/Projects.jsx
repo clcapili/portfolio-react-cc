969aa8c6c4cc5c922a803fa0b0a50ecfe460112a
Charlene <charlenelcapili@gmail.com>
2023-03-16 15:48:21 -0400

    Removed unused Link

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,4 @@
 import { React, useEffect } from 'react'
-import { Link } from 'react-router-dom';
 import ScrollToTopOnLinkClick from './../components/ScrollToTopOnLinkClick';
 
 import FadeInAnimation from './../components/FadeInAnimation';
